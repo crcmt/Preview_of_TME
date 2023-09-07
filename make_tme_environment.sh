c4731932019ff1cc1cf50d5e903a1aa2d999bc21
crcmt <crcmte@protonmail.com>
2023-09-07 08:25:42 +0200

    add VENV_PATH.

--- make_tme_environment.sh
+++ make_tme_environment.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-VENV_NAME="TME_env"
+VENV_NAME="TME_MFP_env"
+VENV_PATH="/opt/$VENV_NAME"
 
-if [ -d "$VENV_NAME" ]
+if [ -d "$VENV_PATH" ]
 then
-    echo "Directory $VENV_NAME already exist."
+    echo "Directory $VENV_PATH already exist."
     echo "If you want to reinstall environment, execute the commands below and then restart script:"
-    echo "rm -rf $VENV_NAME" | tr '[:upper:]' '[:lower:]'
+    echo "rm -rf $VENV_PATH" | tr '[:upper:]' '[:lower:]'
     exit 1
 fi
 
 echo "Create new virtual environment with name: '$VENV_NAME'"
-python3.10 -m venv $VENV_NAME
+python -m venv $VENV_PATH
 
 echo "Enter virtual environment"
-source $VENV_NAME/bin/activate
+source $VENV_PATH/bin/activate
 
 echo "Install packages"
 pip install --upgrade pip wheel --no-cache-dir
